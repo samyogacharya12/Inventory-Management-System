cf0fbbc9a2d7f68e873d1596c42fff0496d30028
samyog <samyog.acharya@gmail.com>
2019-07-23 12:14:05 +0545

    Changed made in Code naming

--- src/main/java/com/example/demo/service/UserDetailServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailServiceImpl.java
@@ -13,12 +13,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.CustomUserDetails;
-import com.example.demo.model.Customer;
 import com.example.demo.model.Customer_Product;
 import com.example.demo.model.Product;
 import com.example.demo.model.Projectuser;
-import com.example.demo.model.Supplier_View;
-import com.example.demo.model.Trash;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.repository.UserRolesRepository;
 
@@ -36,10 +33,10 @@ public class UserDetailServiceImpl implements UserDetailsService {
 	}
 	
 	
-	public void updateExpiredProduct(Product product)
-	{
-		userRepository.updateExpiredProduct(product);
-	}
+//	public void updateExpiredProduct(Product product)
+//	{
+//		userRepository.updateExpiredProduct(product);
+//	}
 	
 	
 	@Override
@@ -56,15 +53,15 @@ public class UserDetailServiceImpl implements UserDetailsService {
 		return new CustomUserDetails(user,roleuser);
 	}
 	
-	public List<Product> findByProductName(String product_name)
-	{
-		return userRepository.findByProductName(product_name);
-	}
+//	public List<Product> findByProductName(String product_name)
+//	{
+//		return userRepository.findByProductName(product_name);
+//	}
 	
 	
 	public void insert(Projectuser projectuser)
 	{
-		userRepository.insert(projectuser);
+		userRepository.Insert(projectuser);
 	}
 	public Projectuser getUserById(long id)
 	{
@@ -76,69 +73,69 @@ public class UserDetailServiceImpl implements UserDetailsService {
 		return userRepository.getAllUserInfo();
 		
 	}
-	public void insertintorpoduct(Product product)
-	{
-		userRepository.insertintoproduct(product);
-	}
-	
-	
-	public void updateintoproduct(Product product)
-	{
-		userRepository.updateintoproduct(product);
-	}
-	
-	public Product getquantitybyid(long id)
-	{
-		return userRepository.getquantitybyid(id);
-	}
-	
-	public List<Product> getAllProductInfo()
-	{
-		return userRepository.getAllProductInfo();
-	}
-	
-	public void UpdateUser(Projectuser projectuser)
-	{
-		userRepository.updateintouser(projectuser);
-	}
-	
-	public void deleteUser(long user_id)
-	{
-		userRepository.deleteuserinfo(user_id);
-	}
-	
-	public Product getproductbyId(long id)
-	{
-		return userRepository.getproductbyid(id);
-	}
-	
-	public void deleteproduct(long product_id)
-	{
-		userRepository.deleteproductinfo(product_id);
-	}
-	
-	public int getTotalNoOfProduct() {
-		
-		return userRepository.getTotalNoOfProduct();
-	}
-	
-	public double getSumOfPrice() {
-		
-		return userRepository.getSumOfPrice();
-	}
-	
-	public int getTotalNoOfQuantity() {
-		return userRepository.getTotalNoOfQuantity();
-	}
-	
-	public int getTotalNoOfProduct(String product_name) {
-		return userRepository.getTotalNoOfProduct(product_name);
-	}
-	
-	public double getSumOfPrice(String product_name) {
-	
-		return userRepository.getSumOfprice(product_name);
-	}
+//	public void insertintorpoduct(Product product)
+//	{
+//		userRepository.insertintoproduct(product);
+//	}
+//
+//
+//	public void updateintoproduct(Product product)
+//	{
+//		userRepository.updateintoproduct(product);
+//	}
+//
+//	public Product getquantitybyid(long id)
+//	{
+//		return userRepository.getquantitybyid(id);
+//	}
+//
+//	public List<Product> getAllProductInfo()
+//	{
+//		return userRepository.getAllProductInfo();
+//	}
+//
+//	public void UpdateUser(Projectuser projectuser)
+//	{
+//		userRepository.updateIntoUser(projectuser);
+//	}
+//
+//	public void deleteUser(long user_id)
+//	{
+//		userRepository.deleteUserInfo(user_id);
+//	}
+//
+//	public Product getproductbyId(long id)
+//	{
+//		return userRepository.getproductbyid(id);
+//	}
+//
+//	public void deleteproduct(long product_id)
+//	{
+//		userRepository.deleteproductinfo(product_id);
+//	}
+//
+//	public int getTotalNoOfProduct() {
+//
+//		return userRepository.getTotalNoOfProduct();
+//	}
+//
+//	public double getSumOfPrice() {
+//
+//		return userRepository.getSumOfPrice();
+//	}
+//
+//	public int getTotalNoOfQuantity() {
+//		return userRepository.getTotalNoOfQuantity();
+//	}
+//
+//	public int getTotalNoOfProduct(String product_name) {
+//		return userRepository.getTotalNoOfProduct(product_name);
+//	}
+//
+//	public double getSumOfPrice(String product_name) {
+//
+//		return userRepository.getSumOfprice(product_name);
+//	}
 	
 
 	public int getTotalNoOfQuantity(String product_name) {
@@ -146,35 +143,35 @@ public class UserDetailServiceImpl implements UserDetailsService {
 	}
 	
 	
-	public Customer_Product getquantitybycustomerid(long customer_id, long product_id)
-	{
-		return userRepository.getquantitybycustomerid(customer_id, product_id);
-	}
-	
-	public boolean createPdf(List<Product> products, ServletContext context, HttpServletRequest request,
-			HttpServletResponse response) {
-		return userRepository.createPdf(products, context, request, response);
-	}
-	 
-	public boolean createExcel(List<Product> products, ServletContext context, HttpServletRequest request,
-			HttpServletResponse response) {
-		
-		return userRepository.createExcel(products, context, request, response);
-	}
-	
-		public int getNoofUsers() 
-		{
-			return userRepository.getNoofUsers();	
-		}
-	
-		public int getNoOfExpiredProduct(String expiry_date) {
-			
-			return userRepository.getNoOfExpiredProduct(expiry_date);
-		}
-		
-		  public List<String> getExpiredProduct(String expiry_date)
-		  {
-			  return userRepository.getExpiredProduct(expiry_date);
-		  }
+//	public Customer_Product getquantitybycustomerid(long customer_id, long product_id)
+//	{
+//		return userRepository.getquantitybycustomerid(customer_id, product_id);
+//	}
+//
+//	public boolean createPdf(List<Product> products, ServletContext context, HttpServletRequest request,
+//			HttpServletResponse response) {
+//		return userRepository.createPdf(products, context, request, response);
+//	}
+//
+//	public boolean createExcel(List<Product> products, ServletContext context, HttpServletRequest request,
+//			HttpServletResponse response) {
+//
+//		return userRepository.createExcel(products, context, request, response);
+//	}
+//
+//		public int getNoofUsers()
+//		{
+//			return userRepository.getNoofUsers();
+//		}
+//
+//		public int getNoOfExpiredProduct(String expiry_date) {
+//
+//			return userRepository.getNoOfExpiredProduct(expiry_date);
+//		}
+//
+//		  public List<String> getExpiredProduct(String expiry_date)
+//		  {
+//			  return userRepository.getExpiredProduct(expiry_date);
+//		  }
 		
 }
