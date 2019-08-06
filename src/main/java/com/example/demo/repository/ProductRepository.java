b772087def7cde97dc13c66e216d74d5f225953c
samyog <samyog.acharya@gmail.com>
2019-08-06 14:55:47 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,33 +1,39 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Customer_Product;
-import com.example.demo.model.Product;
+import com.example.demo.model.*;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface ProductRepository {
 
-    public void insertIntoProduct(Product product);
-    List<Product> findByProductName(String product_name);
-    public void updateExpiredProduct(Product product);
-    Product getProductById(long product_id);
-    public List<Product> getAllProductInfo();
-    public void updateIntoProduct(Product product);
-    public void deleteProductInfo(long product_id);
-    public Product getQuantityById(long id);
-    public Customer_Product getQuantityByCustomerId(long customer_id, long product_id);
-    public int getTotalNoOfProduct();
-    public double getSumOfPrice();
-    public int getTotalNoOfQuantity();
-    public int getTotalNoOfProduct(String product_name);
-    public double getSumOfPrice(String product_name);
-    public int getTotalNoOfQuantity(String product_name);
-    public List<String> getExpiredProduct(String expiry_date);
-    public int getNoOfExpiredProduct(String expiry_date);
-    public boolean createPdf(List<Product> products, ServletContext context, HttpServletRequest request, HttpServletResponse response);
-    public boolean createExcel(List<Product> products, ServletContext context, HttpServletRequest request, HttpServletResponse response);
+     void insertIntoProduct(Product product);
+     void insertIntoProductAnalysis(Map map);
+     List<ProductAnalysis> getAllProductAnalysis();
+     Integer countProductByNameAndType(String productName, String productType);
+//     PurchaseProduct getProductByNameTypeAndPurchaseDate(String productName, String productType, Date purchaseDate);
+     List<PurchaseProduct> getProductByNameTypeAndPurchaseDate(Date purchaseDate);
+     List<PurchaseProduct> getProductByNameTypeLessThanPurchaseDate(String productName, String productType, Date purchaseDate);
+     List<PurchaseProduct> findByProductName(String productName);
+     void updateExpiredProduct(Product product);
+     Product getProductById(long productId);
+     List<PurchaseProduct> getAllProductInfo();
+     void updateIntoProduct(Product product);
+     void deleteProductInfo(long productId);
+//     Product getQuantityById(long id);
+     CustomerProduct getQuantityByCustomerId(long customerId, long productId);
+     int getTotalNoOfProduct();
+     double getSumOfPrice();
+     int getTotalNoOfQuantity();
+     int getTotalNoOfProduct(String productName);
+     double getSumOfPrice(String productName);
+     int getTotalNoOfQuantity(String productName);
+     List<String> getExpiredProduct(String expiryDate);
+     int getNoOfExpiredProduct(String expiryDate);
 
 }
