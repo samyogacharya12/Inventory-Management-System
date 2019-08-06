faee38247444ba3296186c2c5ea12db71fb01554
samyog <samyog.acharya@gmail.com>
2019-08-06 14:54:19 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/service/ProductDetailServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductDetailServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Customer_Product;
-import com.example.demo.model.Product;
+import com.example.demo.model.*;
 import com.example.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +8,10 @@ import org.springframework.stereotype.Service;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductDetailServiceImpl {
@@ -18,106 +20,214 @@ public class ProductDetailServiceImpl {
     @Autowired
     private ProductRepository productRepository;
 
-    public void insertIntoProduct(Product product) {
+    @Autowired
+    private ExcelService excelService;
 
+    @Autowired
+    private PdfService pdfService;
+
+    public void insertIntoProduct(Product product) {
         productRepository.insertIntoProduct(product);
     }
 
+    public List<PurchaseProduct> getProductByNameTypeAndPurchaseDate(Date purchaseDate)
+    {
+        return  productRepository.getProductByNameTypeAndPurchaseDate(purchaseDate);
+    }
 
-    public List<Product> findByProductName(String product_name) {
+//    public PurchaseProduct getProductByNameTypeAndPurchaseDate(String productName, String productType, Date purchaseDate) {
+//        return  productRepository.getProductByNameTypeAndPurchaseDate(productName, productType, purchaseDate);
+//    }
 
-        return productRepository.findByProductName(product_name);
+    public List<PurchaseProduct> getProductByNameTypeLessThanPurchaseDate(String productName, String productType, Date purchaseDate)
+    {
+        return  productRepository.getProductByNameTypeLessThanPurchaseDate(productName, productType, purchaseDate);
+    }
+
+//    public List<Product> getProductByNameTypeAndDate(String productName, String productType, Date purchaseDate) {
+//
+//        return productRepository.getProductByNameTypeAndPurchaseDate(productName, productType, purchaseDate);
+//    }
+
+    public List<ProductAnalysis> getAllProductAnalysis() {
+        return productRepository.getAllProductAnalysis();
+    }
+
+    public void insertIntoProductAnalysis(Map map) {
+        productRepository.insertIntoProductAnalysis(map);
+    }
+
+
+    public Integer countProductByNameAndType(String productName, String productType) {
+        return  productRepository.countProductByNameAndType(productName, productType);
+    }
+
+//    public void calculateDifferenceInProductPrice(String productName, String productType)
+//    {
+//        Date purchaseDate=new Date((System.currentTimeMillis()));
+////        Integer count=countProductByNameAndType(productName, productType);
+////        System.out.println(count);
+////        System.out.println(productName);
+////        System.out.println(productType);
+////        for(int i=0;i<=count;i++)
+////        {
+//            PurchaseProduct purchaseProduct=getProductByNameTypeAndPurchaseDate(productName, productType, purchaseDate);
+//            System.out.println(purchaseProduct);
+////               List<PurchaseProduct> productList=getProductByNameTypeLessThanPurchaseDate(productName, productType, purchaseDate);
+////               System.out.println(productList);
+////
+////                   for(PurchaseProduct productList1:productList)
+////                   {
+////                       if (purchaseProduct.getPrice()>= productList1.getPrice()) {
+////                           Double currentPrice = purchaseProduct.getPrice()-productList1.getPrice();
+////                           Map<String, Object> map = new HashMap<>();
+////                           map.put("productId", purchaseProduct.getProductId());
+////                           map.put("pastPrice", productList1.getPrice());
+////                           map.put("presentPrice", purchaseProduct.getPrice());
+////                           map.put("priceIncreament", currentPrice);
+////                           map.put("priceDecreament", 0);
+////                           map.put("referenceProductId", productList1.getProductId());
+////                           System.out.println(currentPrice);
+////                           insertIntoProductAnalysis(map);
+////                       } else if (productList1.getPrice() == purchaseProduct.getPrice()) {
+////                           System.out.println("The prices of a given product are equal");
+////                       } else if (purchaseProduct.getPrice() <= productList1.getPrice()) {
+////                           Double currentPrice = productList1.getPrice()-purchaseProduct.getPrice();
+////                           Map<String, Object> map = new HashMap<>();
+////                           map.put("productId", purchaseProduct.getProductId());
+////                           map.put("pastPrice", productList1.getPrice());
+////                           map.put("presentPrice", purchaseProduct.getPrice());
+////                           map.put("priceIncreament", 0);
+////                           map.put("priceDecreament", currentPrice);
+////                           map.put("referenceProductId", productList1.getProductId());
+////                           insertIntoProductAnalysis(map);
+////                       } else {
+////                           System.out.println("The given data is empty");
+////                       }
+////                       purchaseDate=productList1.getPurchase_date();
+////
+////                   }
+////               }
+
+        //}
+
+
+
+
+//    public void calculateDiffferenceInProductPrice(long productId,String productName, String productType, double price) {
+//        Date date=new Date((System.currentTimeMillis()));
+//        List<PurchaseProduct> product=getProductByNameTypeAndPurchaseDate(productName, productType,date);
+//        for(PurchaseProduct productValue:product)
+//        {
+//                            if (price >= productValue.getPrice()) {
+//                                Double currentPrice = price - productValue.getPrice();
+//                                Map<String, Object> map = new HashMap<>();
+//                                map.put("productId", productId);
+//                                map.put("pastPrice", productValue.getPrice());
+//                                map.put("presentPrice", price);
+//                                map.put("priceIncreament", currentPrice);
+//                                map.put("priceDecreament", 0);
+//                                map.put("referenceProductId", productValue.getProductId());
+//                                insertIntoProductAnalysis(map);
+//                            } else if (price == productValue.getPrice()) {
+//                                System.out.println("The prices of a given product are equal");
+//                            } else if (productValue.getPrice() >= price) {
+//                                Double currentPrice = productValue.getPrice() - price;
+//                                Map<String, Object> map = new HashMap<>();
+//                                map.put("productId", productId);
+//                                map.put("pastPrice", productValue.getPrice());
+//                                map.put("presentPrice", price);
+//                                map.put("priceIncreament", 0);
+//                                map.put("priceDecreament", currentPrice);
+//                                map.put("referenceProductId", productValue.getProductId());
+//                                insertIntoProductAnalysis(map);
+//                            } else {
+//                                System.out.println("The given data is empty");
+//                            }
+//                        }
+//                }
+
+
+
+
+    public List<PurchaseProduct> findByProductName(String productName) {
+        return productRepository.findByProductName(productName);
     }
 
     public void updateExpiredProduct(Product product) {
-
          productRepository.updateExpiredProduct(product);
     }
 
-    public Product getProductById(long product_id) {
-
-      return  productRepository.getProductById(product_id);
+    public Product getProductById(long productId) {
+      return  productRepository.getProductById(productId);
     }
 
-    public List<Product> getAllProductInfo() {
-
+    public List<PurchaseProduct> getAllProductInfo() {
         return productRepository.getAllProductInfo();
     }
 
 
     public void updateIntoProduct(Product product) {
-
         productRepository.updateIntoProduct(product);
     }
 
-    public void deleteproductinfo(long product_id) {
-
-        productRepository.deleteProductInfo(product_id);
+    public void deleteproductinfo(long productId) {
+        productRepository.deleteProductInfo(productId);
     }
 
-    public Product getQuantityById(long id) {
+//    public Product getQuantityById(long id) {
+//
+//      return productRepository.getQuantityById(id);
+//    }
 
-      return productRepository.getQuantityById(id);
-    }
-
-    public Customer_Product getQuantityByCustomerid(long customer_id, long product_id) {
-
-        return productRepository.getQuantityByCustomerId(customer_id, product_id);
+    public CustomerProduct getQuantityByCustomerid(long customerId, long productId) {
+        return productRepository.getQuantityByCustomerId(customerId, productId);
     }
 
     public int getTotalNoOfProduct() {
-
         return productRepository.getTotalNoOfProduct();
     }
 
     public double getSumOfPrice() {
-
         return productRepository.getSumOfPrice();
     }
 
 
     public int getTotalNoOfQuantity() {
-
         return productRepository.getTotalNoOfQuantity();
     }
 
 
-    public int getTotalNoOfProduct(String product_name) {
-
-        return productRepository.getTotalNoOfProduct(product_name);
+    public int getTotalNoOfProduct(String productName) {
+        return productRepository.getTotalNoOfProduct(productName);
     }
 
 
-    public double getSumOfprice(String product_name) {
-
-        return productRepository.getSumOfPrice(product_name);
+    public double getSumOfPrice(String productName) {
+        return productRepository.getSumOfPrice(productName);
     }
 
-    public int getTotalNoOfQuantity(String product_name) {
-
-        return productRepository.getTotalNoOfQuantity();
+    public int getTotalNoOfQuantity(String productName) {
+        return productRepository.getTotalNoOfQuantity(productName);
     }
 
-    public List<String> getExpiredProduct(String expiry_date) {
-
-        return productRepository.getExpiredProduct(expiry_date);
+    public List<String> getExpiredProduct(String expiryDate) {
+        return productRepository.getExpiredProduct(expiryDate);
     }
 
 
-    public int getNoOfExpiredProduct(String expiry_date) {
-
-        return productRepository.getNoOfExpiredProduct(expiry_date);
+    public int getNoOfExpiredProduct(String expiryDate) {
+        return productRepository.getNoOfExpiredProduct(expiryDate);
     }
 
 
-    public boolean createPdf(List<Product> products, ServletContext context, HttpServletRequest request, HttpServletResponse response) {
-
-        return productRepository.createPdf(products, context, request, response);
+    public boolean createPdfForProducts(List<PurchaseProduct> products, ServletContext context, HttpServletRequest request, HttpServletResponse response) {
+        return pdfService.createPdfForProducts(products, context, request, response);
     }
 
-    public boolean createExcel(List<Product> products, ServletContext context, HttpServletRequest request, HttpServletResponse response) {
+    public boolean createExcelForProducts(List<PurchaseProduct> products, ServletContext context, HttpServletRequest request, HttpServletResponse response) {
 
-        return productRepository.createExcel(products, context, request, response);
+        return excelService.createExcelForProducts(products, context, request, response);
     }
 
 
