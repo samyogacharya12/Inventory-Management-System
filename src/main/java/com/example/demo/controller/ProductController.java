91278caceca6fe850bcc87d5d7b756cae2de6ec2
samyog <samyog.acharya@gmail.com>
2019-08-06 14:54:01 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -7,17 +7,19 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.example.demo.service.ProductDetailServiceImpl;
+import com.example.demo.model.*;
+import com.example.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,10 +29,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.Customer_View;
-import com.example.demo.model.Product;
-import com.example.demo.service.CustomerDetailServiceImpl;
-import com.example.demo.service.TrashDetailServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -48,62 +46,165 @@ public class ProductController {
 	
 	@Autowired
 	private TrashDetailServiceImpl trashDetailService;
-	
+
+	@Autowired
+	private PurchaseDetailServiceImpl purchaseDetailService;
+
+	@Autowired
+	private UserDetailServiceImpl userDetailService;
+
+
+	private UserDetailsService userDetailsService;
+
 	@Autowired
 	private ServletContext context;
+
+
+	@GetMapping("/addProduct")
+	public String getProductForm()
+	{
+		return "AddProduct.jsp";
+	}
+
 	
-	@PostMapping("/save_data")
-	public String insertProduct(HttpServletRequest request,@ModelAttribute Product product, @RequestParam MultipartFile file) throws IOException
+	@PostMapping("/save-product")
+	public String insertProduct(HttpServletRequest request ,@ModelAttribute Product product, @RequestParam MultipartFile file) throws IOException
 	{
 		String imageuploadpath=writeImagetoFile(file);
 		if(product!=null)
 		{
+//			Date[] purchaseDate= {new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis())};
+			Date purchaseDate=new Date((System.currentTimeMillis()));
 			product.setImage(imageuploadpath);
 			productDetailService.insertIntoProduct(product);
+			Map userTempData=userDetailService.getUserTempData();
+			Projectuser projectuser=userDetailService.findByUsername((String) userTempData.get("username"));
+//			productDetailService.calculateDiffferenceInProductPrice(product.getProductId(),product.getProductName(), product.getProductType(), product.getPrice());
+			Map<String, Object> map=new HashMap<>();
+			map.put("userId", projectuser.getUserId());
+			map.put("username", projectuser.getUsername());
+			map.put("productId", product.getProductId());
+			purchaseDetailService.insertIntoPurchase(map);
+			Integer count=productDetailService.countProductByNameAndType(product.getProductName(), product.getProductType());
+			System.out.println(count);
+			for(int i=0;i<=count;i++) {
+				List<PurchaseProduct> purchaseProducts = productDetailService.getProductByNameTypeAndPurchaseDate(purchaseDate);
+				List<PurchaseProduct> productList = productDetailService.getProductByNameTypeLessThanPurchaseDate(product.getProductName(), product.getProductType(), purchaseDate);
+				for (PurchaseProduct purchaseProduct : purchaseProducts) {
+					for (PurchaseProduct productList1 : productList) {
+						if (purchaseProduct.getProductId()!=productList1.getProductId()) {
+							if (purchaseProduct.getPrice() >= productList1.getPrice()) {
+								Double currentPrice = purchaseProduct.getPrice() - productList1.getPrice();
+								Map<String, Object> map1 = new HashMap<>();
+								map1.put("productId", purchaseProduct.getProductId());
+								map1.put("pastPrice", productList1.getPrice());
+								map1.put("presentPrice", purchaseProduct.getPrice());
+								map1.put("priceIncreament", currentPrice);
+								map1.put("priceDecreament", 0);
+								map1.put("referenceProductId", productList1.getProductId());
+								productDetailService.insertIntoProductAnalysis(map1);
+							} else if (productList1.getPrice() == purchaseProduct.getPrice()) {
+								System.out.println("The prices of a given product are equal");
+							} else if (purchaseProduct.getPrice() <= productList1.getPrice()) {
+								Double currentPrice = productList1.getPrice() - purchaseProduct.getPrice();
+								Map<String, Object> map1 = new HashMap<>();
+								map1.put("productId", purchaseProduct.getProductId());
+								map1.put("pastPrice", productList1.getPrice());
+								map1.put("presentPrice", purchaseProduct.getPrice());
+								map1.put("priceIncreament", 0);
+								map1.put("priceDecreament", currentPrice);
+								map1.put("referenceProductId", productList1.getProductId());
+								productDetailService.insertIntoProductAnalysis(map1);
+							} else {
+								System.out.println("The given data is empty");
+							}
+							purchaseDate = productList1.getPurchaseDate();
+							System.out.println(purchaseDate);
+						}
+					}
+				}
+			}
 			request.setAttribute("product_product_id", productDetailService.getAllProductInfo());
 		}
-		return "product.jsp";
+		return "AddProduct.jsp";
 	}
-	@GetMapping("/trashproduct")
-	public String moveToTrash(@RequestParam long product_id)
+
+//	public void calculateDifferenceInProductPrice(String productName, String productType)
+//	{
+//		Date purchaseDate=new Date((System.currentTimeMillis()));
+//        Integer count=productDetailService.countProductByNameAndType(productName, productType);
+//        System.out.println(count);
+//        System.out.println(productName);
+//        System.out.println(productType);
+//        for(int i=0;i<=count;i++) {
+//			List<PurchaseProduct> purchaseProducts = productDetailService.getProductByNameTypeAndPurchaseDate(purchaseDate);
+//			System.out.println(purchaseProducts);
+//			List<PurchaseProduct> productList = productDetailService.getProductByNameTypeLessThanPurchaseDate(productName, productType, purchaseDate);
+//			System.out.println(productList);
+//			for (PurchaseProduct purchaseProduct : purchaseProducts) {
+//				for (PurchaseProduct productList1 : productList) {
+//					if (purchaseProduct.getPrice() >= productList1.getPrice()) {
+//						Double currentPrice = purchaseProduct.getPrice() - productList1.getPrice();
+//						Map<String, Object> map = new HashMap<>();
+//						map.put("productId", purchaseProduct.getProductId());
+//						map.put("pastPrice", productList1.getPrice());
+//						map.put("presentPrice", purchaseProduct.getPrice());
+//						map.put("priceIncreament", currentPrice);
+//						map.put("priceDecreament", 0);
+//						map.put("referenceProductId", productList1.getProductId());
+//						System.out.println(currentPrice);
+//						productDetailService.insertIntoProductAnalysis(map);
+//					} else if (productList1.getPrice() == purchaseProduct.getPrice()) {
+//						System.out.println("The prices of a given product are equal");
+//					} else if (purchaseProduct.getPrice() <= productList1.getPrice()) {
+//						Double currentPrice = productList1.getPrice() - purchaseProduct.getPrice();
+//						Map<String, Object> map = new HashMap<>();
+//						map.put("productId", purchaseProduct.getProductId());
+//						map.put("pastPrice", productList1.getPrice());
+//						map.put("presentPrice", purchaseProduct.getPrice());
+//						map.put("priceIncreament", 0);
+//						map.put("priceDecreament", currentPrice);
+//						map.put("referenceProductId", productList1.getProductId());
+//						productDetailService.insertIntoProductAnalysis(map);
+//					} else {
+//						System.out.println("The given data is empty");
+//					}
+//					purchaseDate = productList1.getPurchaseDate();
+//
+//				}
+//			}
+//		}
+//	}
+
+	@GetMapping("/addToTrash")
+	public String moveToTrash(@RequestParam long productId)
 	{
-		Product product=productDetailService.getProductById(product_id);
-		product.setIs_expired("true");
+		Product product=productDetailService.getProductById(productId);
+		product.setIsExpired("true");
 		productDetailService.updateExpiredProduct(product);
-//		userDetailService.deleteproduct(product_id);
 		trashDetailService.insertIntoTrash(product);
 		return "redirect:/productList.jsp";
 	}
 	
 
-	@GetMapping("/addproduct")
-	public String customer()
-	{
-		return "product.jsp";
-	}
 	
 	
-	@GetMapping(value="/product")
-	public ModelAndView getProductEditForm(@RequestParam long product_id,Model model)
+	@GetMapping(value="/getProductEditForm")
+	public ModelAndView getProductEditForm(@RequestParam long productId,Model model)
 	{
-		Product product=productDetailService.getProductById(product_id);
-	    Date date=product.getMagnifacture_date();
-	    DateFormat dateFormat=new SimpleDateFormat("MM/dd/yyyy");
-	    String date1=dateFormat.format(date);
-	    model.addAttribute("magnifacture_date", date1);
-	    System.out.println(date1);
+		Product product=productDetailService.getProductById(productId);
 		return new ModelAndView("productEdit.jsp", "productEdit", product);
 	}
 	
-	@PostMapping("/update_product")
+	@PostMapping("/update-product")
 	public String updateproduct(@ModelAttribute Product product,@RequestParam MultipartFile file ) throws IOException
 	{
 		String imageuploadpath="";
 		if(file.getOriginalFilename().isEmpty())
 		{
-			Product prod=productDetailService.getProductById(product.getProduct_id());
-			System.out.println(product.getMagnifacture_date());
-			System.out.println(product.getExpiry_date());
+			Product prod=productDetailService.getProductById(product.getProductId());
+			System.out.println(product.getMagnifactureDate());
+			System.out.println(product.getExpiryDate());
 			imageuploadpath=prod.getImage();
 		}
 		else
@@ -119,59 +220,59 @@ public class ProductController {
 		return "redirect:/productEdit.jsp";
 	}
 	
-	@GetMapping("/list_product")
-	public ModelAndView List_Product(Model model)
+	@GetMapping("/list-product")
+	public ModelAndView listProduct(Model model)
 	{
-	  List<Product> product=productDetailService.getAllProductInfo();
+	  List<PurchaseProduct> purchaseProducts=productDetailService.getAllProductInfo();
 	  int totalquantity=productDetailService.getTotalNoOfQuantity();
 	  int totalnoproduct=productDetailService.getTotalNoOfProduct();
 	  double totalnoprice=productDetailService.getSumOfPrice();
 	  model.addAttribute("quantity", totalquantity);
 	  model.addAttribute("totalproduct", totalnoproduct);
 	  model.addAttribute("price", totalnoprice);
-	  return new ModelAndView("productList.jsp", "product", product);
+	  return new ModelAndView("productList.jsp", "purchaseProducts", purchaseProducts);
 	}
 	
 	
 	@GetMapping("/getProductByName")
-	public ModelAndView get_product(Model model,@RequestParam String product_name) throws IOException
+	public ModelAndView getProduct(Model model,@RequestParam String productName) throws IOException
 	{
-		System.out.println(product_name);
-		List<Product> findproduct=productDetailService.findByProductName(product_name);
-		int totalquantity=productDetailService.getTotalNoOfQuantity(product_name);
+		System.out.println(productName);
+		List<PurchaseProduct> findproduct=productDetailService.findByProductName(productName);
+		int totalquantity=productDetailService.getTotalNoOfQuantity(productName);
 		System.out.println(totalquantity);
 		model.addAttribute("totalquantity1", totalquantity);
-		int totalproduct=productDetailService.getTotalNoOfProduct(product_name);
+		int totalproduct=productDetailService.getTotalNoOfProduct(productName);
 		System.out.println(totalproduct);
 		model.addAttribute("totalproduct1", totalproduct);
-		double totalnoprice=productDetailService.getSumOfprice(product_name);
+		double totalnoprice=productDetailService.getSumOfPrice(productName);
 		model.addAttribute("price1", totalnoprice);
 		return new ModelAndView("productList.jsp", "producter", findproduct);
 	}
 	
-	@GetMapping(value="/deleteproduct")
-	public String deleteProduct(@RequestParam long product_id)
+	@GetMapping(value= "/delete-product")
+	public String deleteProduct(@RequestParam long productId)
 	{
-		productDetailService.deleteproductinfo(product_id);
+		productDetailService.deleteproductinfo(productId);
 		return "redirect:/productList.jsp";
 	}
 	
 	
 	@GetMapping(value="/viewData")
-	public String viewData(@RequestParam(value="sell_date[]") String[] sell_date, Model model) throws JsonProcessingException
+	public String viewData(@RequestParam(value="sellDate[]") String[] sellDate, Model model) throws JsonProcessingException
 	{
-		List<Customer_View> customerView=customerDetailService.getCustomerBuyDate(sell_date);
+		List<CustomerView> customerView=customerDetailService.getCustomerBuyDate(sellDate);
 		ObjectMapper objectMapper=new ObjectMapper();
 		model.addAttribute("sales", objectMapper.writeValueAsString(customerView));
-		model.addAttribute("date", sell_date);
+		model.addAttribute("date", sellDate);
 		return "redirect:/chart.jsp";
 	}
  
 	@GetMapping(value="/createExcelProduct")
 	public void createExcel(HttpServletRequest request, HttpServletResponse response)
 	{
-		List<Product> products=productDetailService.getAllProductInfo();
-		boolean isflag=productDetailService.createExcel(products, context, request, response);
+		List<PurchaseProduct> products=productDetailService.getAllProductInfo();
+		boolean isflag=productDetailService.createExcelForProducts(products, context, request, response);
 		if(isflag)
 		{
 			String filepath=request.getServletContext().getRealPath("/resources/reports/"+"products"+".xls");
@@ -183,8 +284,8 @@ public class ProductController {
 	@GetMapping(value="/createPdfProduct")
 	public void createPdf(HttpServletRequest request, HttpServletResponse response)
 	{
-		List<Product> products=productDetailService.getAllProductInfo();
-		boolean isflag=productDetailService.createPdf(products, context,request, response);
+		List<PurchaseProduct> products=productDetailService.getAllProductInfo();
+		boolean isflag=productDetailService.createPdfForProducts(products, context,request, response);
 		if(isflag)
 		{
 			String fullpath=request.getServletContext().getRealPath("/resources/reports/"+"products"+".pdf");
