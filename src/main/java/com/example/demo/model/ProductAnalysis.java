8594478f789f42b48a32627f48af2fc8cc90b578
samyog <samyog.acharya@gmail.com>
2019-08-06 15:10:36 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

---
+++ src/main/java/com/example/demo/model/ProductAnalysis.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+public class ProductAnalysis {
+
+    private long productId;
+    private double pastPrice;
+    private double presentPrice;
+    private double priceIncreament;
+    private double priceDecreament;
+    private long referenceProductId;
+
+    public double getPresentPrice() {
+        return presentPrice;
+    }
+
+    public void setPresentPrice(double presentPrice) {
+        this.presentPrice = presentPrice;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public double getPastPrice() {
+        return pastPrice;
+    }
+
+    public void setPastPrice(double pastPrice) {
+        this.pastPrice = pastPrice;
+    }
+
+    public double getPriceIncreament() {
+        return priceIncreament;
+    }
+
+    public void setPriceIncreament(double priceIncreament) {
+        this.priceIncreament = priceIncreament;
+    }
+
+    public double getPriceDecreament() {
+        return priceDecreament;
+    }
+
+    public void setPriceDecreament(double priceDecreament) {
+        this.priceDecreament = priceDecreament;
+    }
+
+    public long getReferenceProductId() {
+        return referenceProductId;
+    }
+
+    public void setReferenceProductId(long referenceProductId) {
+        this.referenceProductId = referenceProductId;
+    }
+}
