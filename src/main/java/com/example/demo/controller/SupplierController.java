11a58802f7360a09586042d5b787405b2cf9dd29
samyog <samyog.acharya@gmail.com>
2019-08-06 15:03:03 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/controller/SupplierController.java
+++ src/main/java/com/example/demo/controller/SupplierController.java
@@ -8,11 +8,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.demo.model.*;
 import com.example.demo.service.ProductDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,10 +26,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.Product;
-import com.example.demo.model.Supplier;
-import com.example.demo.model.Supplier_Product;
-import com.example.demo.model.Supplier_View;
 import com.example.demo.service.CustomerDetailServiceImpl;
 import com.example.demo.service.SupplierDetailService;
 import com.example.demo.service.UserDetailServiceImpl;
@@ -36,7 +34,7 @@ import com.example.demo.service.UserDetailServiceImpl;
 public class SupplierController {
 
 	String uploaded_file="C:/Users/DELL/Desktop/New Files/";
-	
+
 	@Autowired
 	private SupplierDetailService supplierDetailService;
 	@Autowired
@@ -49,33 +47,33 @@ public class SupplierController {
 
 	@Autowired
 	private CustomerDetailServiceImpl customerDetailServiceImpl;
-	@GetMapping("/supplier_Form")
+	@GetMapping("/getSupplierForm")
 	public String getForm()
-	{	
+	{
 		return "supplierForm.jsp";
 	}
 
-	@GetMapping("/getSupplier")
-	public ModelAndView getSupplierId(@RequestParam long supplier_id, Model model)
+	@GetMapping("/getSupplierProductAddForm")
+	public ModelAndView getSupplierAddForm(@RequestParam long supplierId, Model model)
 	{
-		List<Product> product1=productDetailService.getAllProductInfo();
+		List<PurchaseProduct> product1=productDetailService.getAllProductInfo();
 		System.out.println(product1);
         model.addAttribute("product1", product1);
-		Supplier supplier=supplierDetailService.getSupplierId(supplier_id);
+		Supplier supplier=supplierDetailService.getSupplierId(supplierId);
 		return new ModelAndView("supplierproductAdd.jsp", "supplierAdd", supplier);
 	}
-	
+
 	@GetMapping("/addSupplierProduct")
-	public ModelAndView addSupplierProduct(@RequestParam long supplier_id, Model model)
+	public ModelAndView addSupplierProduct(@RequestParam long supplierId, Model model)
 	{
-		List<Product> product1=productDetailService.getAllProductInfo();
+		List<PurchaseProduct> product1=productDetailService.getAllProductInfo();
 		System.out.println(product1);
         model.addAttribute("product1", product1);
-		Supplier supplier=supplierDetailService.getSupplierId(supplier_id);
+		Supplier supplier=supplierDetailService.getSupplierId(supplierId);
 		return new ModelAndView("supplierproductAdd.jsp", "supplierAdd", supplier);
 	}
-	
-	@PostMapping("/save_supplier")
+
+	@PostMapping("/save-supplier")
 	public String postForm(HttpServletRequest request, @ModelAttribute Supplier supplier, @RequestParam MultipartFile file) throws IOException
 	{
 		String imageuploadpath=writeImagetoFile(file);
@@ -83,26 +81,28 @@ public class SupplierController {
 		{
 			supplier.setImage(imageuploadpath);
 			supplierDetailService.insertIntoSupplier(supplier);
-		}	
+		}
 		return "supplierForm.jsp";
 	}
-	
-	@PostMapping("/add_supplierproduct")
-	public String postSupplierProductForm(@ModelAttribute Supplier_Product supplierproduct, @RequestParam long product_id)
-	{	
-		Product product=productDetailService.getQuantityById(product_id);
+
+	@PostMapping("/save-supplierproduct")
+	public String insertIntoSupplierProduct(@ModelAttribute SupplierProduct supplierproduct, @RequestParam long productId)
+	{
+		Map map=userDetailService.getUserTempData();
+		Product product=productDetailService.getProductById(productId);
 		int quantity=product.getQuantity();
 		supplierproduct.setQuantity(quantity);
+		supplierproduct.setUsername((String) map.get("username"));
 		supplierDetailService.insertIntoSupplierProduct(supplierproduct);
-    	return "redirect:/supplierproductAdd.jsp"; 	
+    	return "redirect:/supplierproductAdd.jsp";
 	}
-	
-	
-	@GetMapping("/list_supplier")
+
+
+	@GetMapping("/list-supplier")
 	public ModelAndView getSupplier(Model model)
-	{	
-		List<Supplier_View> supplier=supplierDetailService.getAllSupplierInfo();
-		List<Supplier_View> supplier1=supplierDetailService.getSupplierInformation();
+	{
+		List<SupplierView> supplier=supplierDetailService.getAllSupplierInfo();
+		List<SupplierView> supplier1=supplierDetailService.getSupplierInformation();
 		System.out.println(supplier1);
 		 double supplier2=supplierDetailService.getSupplierCost();
 		 int supplier3=supplierDetailService.getNoofSupplier();
@@ -115,40 +115,40 @@ public class SupplierController {
 		model.addAttribute("supplier7", supplier5);
 		return new ModelAndView("supplierList.jsp", "supplier", supplier);
 	}
-	
-	@GetMapping("/supplier")
-	public ModelAndView getSupplierEditForm(@RequestParam long supplier_id, @RequestParam long product_id)
+
+	@GetMapping("/getSupplierBySupplierIdAndProductId")
+	public ModelAndView getSupplierEditForm(@RequestParam long supplierId, @RequestParam long productId)
 	{
-		Supplier_View supplier=supplierDetailService.getSupplierBySupplierIdAndProductId(supplier_id, product_id);
-		return new ModelAndView("supplierEdit.jsp", "supplierEdit", supplier);	
+		SupplierView supplier=supplierDetailService.getSupplierBySupplierIdAndProductId(supplierId, productId);
+		return new ModelAndView("supplierEdit.jsp", "supplierEdit", supplier);
 	}
-	
-	@GetMapping("/supplierEdit")
-	public ModelAndView getSupplierEditForm(@RequestParam long supplier_id)
+
+	@GetMapping("/getSupplierEditForm")
+	public ModelAndView getSupplierEditForm(@RequestParam long supplierId)
 	{
-		Supplier supplier=supplierDetailService.getSupplierId(supplier_id);
+		Supplier supplier=supplierDetailService.getSupplierId(supplierId);
 		return new ModelAndView("supplierpersonalEdit.jsp", "supplierEdit", supplier);
-		
+
 	}
-    
-	
-	@GetMapping("/supplierproduct")
-	public ModelAndView getSupplierproductEditForm(@RequestParam long supplier_id, @RequestParam long product_id)
+
+
+	@GetMapping("/getSupplierProductEditForm")
+	public ModelAndView getSupplierproductEditForm(@RequestParam long supplierId, @RequestParam long productId)
 	{
-		System.out.println(supplier_id);
-		System.out.println(product_id);
-		Supplier_View supplier=supplierDetailService.getSupplierBySupplierIdAndProductId(supplier_id, product_id);
+		System.out.println(supplierId);
+		System.out.println(productId);
+		SupplierView supplier=supplierDetailService.getSupplierBySupplierIdAndProductId(supplierId, productId);
 		return new ModelAndView("supplierproductEdit.jsp","supplierproduct",supplier);
 	}
-	
+
 	@GetMapping("/supplierproductadd")
 	public ModelAndView AddSupplierProductForm()
 	{
 		List<Supplier> supplier=supplierDetailService.getAllSupplier();
 		return new ModelAndView("supplierproduct.jsp", "supplier", supplier);
 	}
-	
-	@PostMapping("/update_supplierpersonal")
+
+	@PostMapping("/update-supplierpersonal")
 	public String updateSupplierData(@ModelAttribute Supplier suppliers, @RequestParam long supplier_id, @RequestParam MultipartFile file) throws IOException
 	{
 		String imageuploadpath="";
@@ -164,65 +164,65 @@ public class SupplierController {
 		if(suppliers!=null)
 		{
 			suppliers.setImage(imageuploadpath);
-			System.out.println(suppliers.getSupplier_name());
+			System.out.println(suppliers.getSupplierName());
             supplierDetailService.updateIntoSupplier(suppliers);
-            System.out.println(suppliers.getSupplier_name());
+            System.out.println(suppliers.getSupplierName());
 		}
 		return "redirect:/supplierpersonalEdit.jsp";
 	}
-	
-	
-	@PostMapping("/update_supplier")
-	public String updateSupplier(@ModelAttribute Supplier suppliers,@RequestParam long supplier_id,@RequestParam MultipartFile file) throws IOException
+
+
+	@PostMapping("/update-supplierproduct")
+	public String updateSupplierProduct(@ModelAttribute Supplier suppliers,@RequestParam long supplierId,@RequestParam MultipartFile file) throws IOException
 	{
 		String imageuploadpath="";
 		if(file.getOriginalFilename().isEmpty())
      {
-	      Supplier supplier=supplierDetailService.getSupplierId(supplier_id);
+	      Supplier supplier=supplierDetailService.getSupplierId(supplierId);
 	      imageuploadpath=supplier.getImage();
      }
 		else
 		{
 			imageuploadpath=writeImagetoFile(file);
 		}
-		
+
 		if(suppliers!=null)
 		{
 			suppliers.setImage(imageuploadpath);
 			supplierDetailService.updateIntoSupplier(suppliers);
 		}
-		
+
 		return "redirect:/supplierEdit.jsp";
-		
+
 	}
-	
+
 	@PostMapping("/update_supplierproduct")
-	public String updateSupplierProduct(@ModelAttribute Supplier_Product supplierproduct, @RequestParam long supplier_id)
+	public String updateSupplierProduct(@ModelAttribute SupplierProduct supplierproduct, @RequestParam long supplierId)
 	{
 		supplierDetailService.updateIntoSupplierView(supplierproduct);
 		return "redirect:/supplierproductEdit.jsp";
 	}
-	
-	@GetMapping("/deletesupplier")
-	public String deleteSupplier(@RequestParam long supplier_id, @RequestParam long product_id)
+
+	@GetMapping("/delete-supplier")
+	public String deleteSupplier(@RequestParam long supplierId, @RequestParam long productId)
 	{
-		supplierDetailService.deleteIntoSupplierView(supplier_id, product_id);
-		supplierDetailService.deleteIntoSupplier(supplier_id, product_id);
+		supplierDetailService.deleteIntoSupplierView(supplierId, productId);
+		supplierDetailService.deleteIntoSupplier(supplierId, productId);
 		return "supplierList.jsp";
 	}
-	
-	
-	@GetMapping("/getDate")
-	public ModelAndView getSupplier(Model model ,@RequestParam(value="buy_date[]") String buy_date[])
+
+
+	@GetMapping("/getSupplierByDate")
+	public ModelAndView getSupplierByDate(Model model ,@RequestParam(value="buyDate[]") String buyDate[])
 	{
-		System.out.println(buy_date.toString());
-		List<Supplier_View> supplier=supplierDetailService.getSupplierInformation();
+		System.out.println(buyDate.toString());
+		List<SupplierView> supplier=supplierDetailService.getSupplierInformation();
 		model.addAttribute("supplier2", supplier);
-	List<Supplier_View> supplierproduct=supplierDetailService.getSupplierByBuyDate(buy_date);
-	
-	int totalsupplier=supplierDetailService.getNoofSupplier(buy_date);
+	List<SupplierView> supplierproduct=supplierDetailService.getSupplierByBuyDate(buyDate);
+
+	int totalsupplier=supplierDetailService.getNoofSupplier(buyDate);
 	model.addAttribute("totalsupplier1", totalsupplier);
-	Integer totalquantity=supplierDetailService.getNoofQuantity(buy_date);
+	Integer totalquantity=supplierDetailService.getNoofQuantity(buyDate);
 	System.out.println(totalquantity);
 	if(totalquantity==null)
 	{
@@ -234,7 +234,7 @@ public class SupplierController {
 	{
 	model.addAttribute("totalquantity1", totalquantity);
 	}
-	Double totalcost=supplierDetailService.getSumofCost(buy_date);
+	Double totalcost=supplierDetailService.getSumofCost(buyDate);
 	if(totalcost==null)
 	{
 		totalcost=(double) 0;
@@ -244,44 +244,44 @@ public class SupplierController {
 	{
 		model.addAttribute("totalcost1", totalcost);
 	}
-	int totalproduct=supplierDetailService.getNoofProduct(buy_date);
+	int totalproduct=supplierDetailService.getNoofProduct(buyDate);
 	model.addAttribute("totalproduct1", totalproduct);
-	
-		return new ModelAndView("supplierList.jsp", "begin_date", supplierproduct);
+
+		return new ModelAndView("supplierList.jsp", "getSupplierByDate", supplierproduct);
 	}
-	
+
 	@GetMapping("/getSupplierByName")
-	public ModelAndView getSupplierByName(@RequestParam String supplier_name,Model model)
+	public ModelAndView getSupplierByName(@RequestParam String supplierName,Model model)
 	{
-		System.out.println(supplier_name);
-		List<Supplier_View> supplier1=supplierDetailService.getSupplierInformation();
+		System.out.println(supplierName);
+		List<SupplierView> supplier1=supplierDetailService.getSupplierInformation();
 		model.addAttribute("supplier1", supplier1);
-		List<Supplier_View> findSupplier=supplierDetailService.getSupplierByName(supplier_name);
-        double totalproductcost=supplierDetailService.getSumofCost(supplier_name);
+		List<SupplierView> findSupplier=supplierDetailService.getSupplierByName(supplierName);
+        double totalproductcost=supplierDetailService.getSumofCost(supplierName);
         model.addAttribute("totalcost", totalproductcost);
-        int totalsupplier=supplierDetailService.getNoofSupplier(supplier_name);
+        int totalsupplier=supplierDetailService.getNoofSupplier(supplierName);
         System.out.println(totalsupplier);
         model.addAttribute("totalsupplier", totalsupplier);
-        int totalquantity=supplierDetailService.getNoofQuantity(supplier_name);
+        int totalquantity=supplierDetailService.getNoofQuantity(supplierName);
         model.addAttribute("totalquantity", totalquantity);
-        int totalproduct=supplierDetailService.getNoofProduct(supplier_name);
+        int totalproduct=supplierDetailService.getNoofProduct(supplierName);
         model.addAttribute("totalproduct", totalproduct);
-		return new ModelAndView("supplierList.jsp", "supply", findSupplier);
+		return new ModelAndView("supplierList.jsp", "getSupplierByName", findSupplier);
 	}
-	
-	
-	@GetMapping("/supplier_Report")
+
+
+	@GetMapping("/getSupplierPersonalInfo")
 	public ModelAndView getSupplierPersonalInfo()
 	{
 		List<Supplier> supplier=supplierDetailService.getAllSupplier();
 		return new ModelAndView("supplierpersonal.jsp", "supplierinfo", supplier);
 	}
-	
+
 	@GetMapping(value="/createExcelSupplier")
 	public void createExcelSupplier(HttpServletRequest request, HttpServletResponse response)
 	{
-		List<Supplier_View> supplierproducts=supplierDetailService.getAllSupplierInfo();
-		boolean isflag=supplierDetailService.createExcel(supplierproducts, context, response, request);
+		List<SupplierView> supplierproducts=supplierDetailService.getAllSupplierInfo();
+		boolean isflag=supplierDetailService.createExcelForSuppliers(supplierproducts, context, response, request);
 		if(isflag)
 		{
 			String filepath=request.getServletContext().getRealPath("/resources/reports/"+"suppliers"+".xls");
@@ -291,17 +291,17 @@ public class SupplierController {
 	@GetMapping(value="/createPdfSupplier")
 	public void createPdf(HttpServletRequest request, HttpServletResponse response)
 	{
-		List<Supplier_View> supplierproducts=supplierDetailService.getAllSupplierInfo();
-		boolean isflag=supplierDetailService.createPdf(supplierproducts, context, response, request);
+		List<SupplierView> supplierproducts=supplierDetailService.getAllSupplierInfo();
+		boolean isflag=supplierDetailService.createPdfForSuppliers(supplierproducts, context, response, request);
 		if(isflag)
 		{
 			String filepath=request.getServletContext().getRealPath("/resources/reports/"+"suppliers"+".pdf");
 			filedownload(filepath, response, "suppliers.pdf");
 	}
 	}
-	
-	
-	private void filedownload(String fullpath, HttpServletResponse response, String filename) 
+
+
+	private void filedownload(String fullpath, HttpServletResponse response, String filename)
 	{
 		// TODO Auto-generated method stub
 		File file=new File(fullpath);
@@ -322,18 +322,16 @@ public class SupplierController {
 	}
 				inputstream.close();
 				outputStream.close();
-				file.delete();				
+				file.delete();
 			}
 			catch(Exception e)
 			{
 				e.printStackTrace();
 			}
 		}
-		
-	}
-	
 
-	
+	}
+
 	public String writeImagetoFile(MultipartFile file) throws IOException
 	{
 		String imageuploadPath=uploaded_file +file.getOriginalFilename();
@@ -349,9 +347,9 @@ public class SupplierController {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 		return imageuploadPath;
 	}
-	
+
 	
 }
