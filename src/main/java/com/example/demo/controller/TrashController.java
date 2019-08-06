2afd34dfa909e063497568683e6a25f31fd7c3a9
samyog <samyog.acharya@gmail.com>
2019-08-06 15:07:47 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/controller/TrashController.java
+++ src/main/java/com/example/demo/controller/TrashController.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.Customer_View;
 import com.example.demo.model.Trash;
 import com.example.demo.service.TrashDetailServiceImpl;
 
@@ -18,7 +16,7 @@ public class TrashController {
 	@Autowired
 	private TrashDetailServiceImpl trashDetailService;
 	
-	  @GetMapping("/get_trash")
+	  @GetMapping("/list-trash")
 	  public ModelAndView getTrashReport()
 	  {
 		  List<Trash> trash=trashDetailService.getAllTrashInfo();
