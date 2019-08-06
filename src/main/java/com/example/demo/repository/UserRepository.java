52fb32e0eb57dfbe377500990bf1067f3cb8ba7d
samyog <samyog.acharya@gmail.com>
2019-08-06 15:09:37 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,21 +1,15 @@
 package com.example.demo.repository;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.example.demo.model.Customer_Product;
-import com.example.demo.model.Customer_View;
-import com.example.demo.model.Product;
 import com.example.demo.model.Projectuser;
-import com.example.demo.model.Supplier_View;
-import com.example.demo.model.Trash;
+import com.example.demo.model.Usertemp;
 
 public interface UserRepository {
 
   Projectuser findByUsername(String username);
+  Projectuser getUserIdByUsername(String username);
 //  List<Product> findByProductName(String product_name);
   public void Insert(Projectuser projectuser);
 //  public void updateExpiredProduct(Product product);
@@ -26,15 +20,18 @@ public interface UserRepository {
 //  public List<Product> getAllProductInfo();
 //  public void insertintoproduct(Product product);
   public void updateIntoUser(Projectuser user);
+  void insetIntoUserTemp(Map map);
+  Map getUserTempData();
+  void deleteIntoUserTemp(String username);
 //  public void updateintoproduct(Product product);
 //  public void deleteproductinfo(long product_id);
 //  public Product getquantitybyid(long id);
-//  public Customer_Product getquantitybycustomerid(long customer_id, long product_id);
+//  public CustomerProduct getquantitybycustomerid(long customer_id, long product_id);
 //  public int getTotalNoOfProduct();
 //  public double getSumOfPrice();
 //  public int getTotalNoOfQuantity();
 //  public int getTotalNoOfProduct(String product_name);
-//  public double getSumOfprice(String product_name);
+//  public double getSumOfPrice(String product_name);
   public int getTotalNoOfQuantity(String product_name);
   public int getNoofUsers();
 //  public List<String> getExpiredProduct(String expiry_date);
