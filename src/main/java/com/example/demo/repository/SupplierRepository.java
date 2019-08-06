345809c5c6b44f2b8073b639e8cf52204d2e758a
samyog <samyog.acharya@gmail.com>
2019-08-06 15:06:49 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/repository/SupplierRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRepository.java
@@ -1,46 +1,43 @@
 package com.example.demo.repository;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.example.demo.model.Supplier;
-import com.example.demo.model.Supplier_Product;
-import com.example.demo.model.Supplier_View;
+import com.example.demo.model.SupplierProduct;
+import com.example.demo.model.SupplierView;
 
 public interface SupplierRepository {
 
-	public void insertIntoSupplierProduct(Supplier_Product supplierproduct);
-	public void insertIntoSupplier(Supplier supplier);
-    public List<Supplier_View> getAllSupplierInfo();
-    public Supplier_View getSupplierBySupplierIdAndProductId(long supplier_id, long product_id);
-    public Supplier getSupplierId(long supplier_id);
-    public void updateIntoSupplier(Supplier supplier);
-    public void updateIntoSupplierView(Supplier_Product supplierview);
-    public void deleteIntoSupplierView(long supplier_id, long product_id);
-    public void deleteIntoSupplier(long supplier_id, long product_id);
-    public List<Supplier_View> getSupplierByBuyDate(String[] buy_date);
-    public List<Supplier> getAllSupplier();
-    public List<Supplier_View> getSupplierByName(String supplier_name);
-    public List<Supplier_View> getSupplierInformaton();
-    public double getSumOfCost();
-    public int getNoofSupplier();
-    public int getNoofQuantity();
-    public int getNoofProduct();
-    public int NoofSupplier();
-    public double getSumofCost(String supplier_name);
-    public int getNoofSupplier(String supplier_name);
-    public int getNoofQuantity(String supplier_name);
-    public int getNoofProduct(String supplier_name);
-    public Double getSumofCost(String[] buy_date);
-    public int getNoofSupplier(String[] buy_date);
-    public Integer getNoofQuantity(String[] buy_date);
-    public int getNoofProduct(String[] buy_date);
-    public boolean createExcel(List<Supplier_View> supplierproducts, ServletContext context, HttpServletResponse response, HttpServletRequest request);
-    public boolean createPdf(List<Supplier_View> supplierproducts, ServletContext context, HttpServletResponse response, HttpServletRequest request);
+	 void insertIntoSupplierProduct(SupplierProduct supplierproduct);
+	 void insertIntoSupplier(Supplier supplier);
+     List<SupplierView> getAllSupplierInfo();
+     SupplierView getSupplierBySupplierIdAndProductId(long supplierId, long productId);
+     Supplier getSupplierId(long supplierId);
+     void updateIntoSupplier(Supplier supplier);
+     void updateIntoSupplierView(SupplierProduct supplierview);
+     void deleteIntoSupplierView(long supplierId, long productId);
+     void deleteIntoSupplier(long supplierId, long productId);
+     List<SupplierView> getSupplierByBuyDate(String[] buyDate);
+     List<Supplier> getAllSupplier();
+     List<SupplierView> getSupplierByName(String supplierName);
+     List<SupplierView> getSupplierInformaton();
+     double getSumOfCost();
+     int getNoofSupplier();
+     int getNoofQuantity();
+     int getNoofProduct();
+     int NoofSupplier();
+     double getSumofCost(String supplierName);
+     int getNoofSupplier(String supplierName);
+     int getNoofQuantity(String supplierName);
+     int getNoofProduct(String supplierName);
+    Double getCostFromExpiredProduct(String[] buyDate);
+    Double getSumofCost(String[] buyDate);
+    int getNoofSupplier(String[] buyDate);
+    Integer getNoofQuantity(String[] buyDate);
+     int getNoofProduct(String[] buyDate);
 }
 
