125e96651afcc33fd36392bdcd2f17bc94571ad6
samyog <samyog.acharya@gmail.com>
2019-08-06 15:11:10 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

---
+++ src/main/java/com/example/demo/repository/PurchaseRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Purchase;
+
+import java.util.Map;
+
+public interface PurchaseRepository {
+
+    void insertIntoPurchase(Map map);
+}
