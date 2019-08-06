f341ba0caab361a8116857d2b003bdaecd49b248
samyog <samyog.acharya@gmail.com>
2019-08-06 14:30:40 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

---
+++ src/main/java/com/example/demo/model/CustomerProduct.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class CustomerProduct {
+
+	private long customerPurchaseId;
+	private long customerId;
+	private long productId;
+    private int quantity;
+    private double amount;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date buyDate;
+	private String username;
+
+	public CustomerProduct()
+	{
+
+	}
+	public long getCustomerPurchaseId() {
+		return customerPurchaseId;
+	}
+	public void setCustomerPurchaseId(long customerPurchaseId) {
+		this.customerPurchaseId = customerPurchaseId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getBuyDate() {
+		return buyDate;
+	}
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+}
