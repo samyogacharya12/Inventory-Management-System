b62b8bc2fc00ac30c9eed0623d12251c78127d9f
samyog <samyog.acharya@gmail.com>
2019-08-06 14:32:46 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/SpringSecurity-Jdbc
    # Conflicts:
    #	src/main/java/com/example/demo/controller/ProductController.java
    #	src/main/java/com/example/demo/controller/SpringSecurityJdbcController.java
    #	src/main/java/com/example/demo/repository/TrashRepository.java
    #	src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
    #	src/main/java/com/example/demo/repository/UserRepository.java
    #	src/main/java/com/example/demo/repository/UserRepositoryImpl.java
    #	src/main/java/com/example/demo/service/TrashDetailServiceImpl.java
    #	src/main/java/com/example/demo/service/UserDetailServiceImpl.java

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,44 +1,46 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.example.demo.model.Customer;
-import com.example.demo.model.Customer_Product;
-import com.example.demo.model.Customer_View;
+import com.example.demo.model.CustomerProduct;
+import com.example.demo.model.CustomerView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface CustomerRepository {
 
-	public List<Customer> getAllCustomer();
-	public Customer_Product getCustomerById(long customer_id);
-	public Customer_View getCustomerId(long customer_id, long product_id);
-	public List<Customer_View> getAllCustomerInfo();
-	 public void insertIntoCustomer(Customer customer);
-	  public void insertIntoCustomerProduct(Customer_Product customerproduct);
-	  public void updateIntoPersonalCustomer(Customer customer);
-	  public void updateIntoCustomerProduct(Customer_Product customerproduct);
-	  public void deleteIntoCustomerView(long customer_id, long product_id);
-	  public void deleteIntoCustomer(long customer_id, long product_id);
-	  public List<Customer_View> getCustomerByBuyDate(String[] buy_date);
-	  public List<Customer_View> getCustomerByName(String customer_name);
-	  public int getTotalCustomer();
-	  public int sumOfQuantity();
-	  public double sumOfAmount();
-	  public int getTotalProduct();
-	  public int getTotalCustomer(String customer_name);
-	  public int sumOfQuantity(String customer_name);
-	  public double sumOfAmount(String customer_name);
-	  public int getTotalProduct(String customer_name);
-	  public int getTotalCustomer(String[] buy_date);
-	  public Integer sumOfQuantity(String[] buy_date);
-	  public Double sumOfAmount(String[] buy_date);
-	  public int getTotalProduct(String[] buy_date);
-	  public int getPresentDate(String buy_date);
-	  public int getNumberofCustomersToday(String buy_date);
-	  public Double getPresentRevenue(String buy_date);
-	  public boolean createPdf(List<Customer_View> customers, ServletContext context, HttpServletRequest request, HttpServletResponse response);
-	  public boolean createExcel(List<Customer_View> customers, ServletContext context, HttpServletRequest request, HttpServletResponse response);
+	 List<CustomerView> getAllCustomer();
+	 CustomerProduct getCustomerById(long customerId);
+	 List<Integer> getProductIdNotEqualToCustomerId(long customerId);
+	 CustomerView getCustomerId(long customerId, long productId);
+	 CustomerView getDataByCustomerId(long customerId ,long customerPurchaseId);
+	 List<CustomerView> getAllCustomerInfo();
+	  void insertIntoCustomer(Customer customer);
+	   void insertIntoCustomerProduct(CustomerProduct customerproduct);
+	   void updateIntoPersonalCustomer(Customer customer);
+	   void updateIntoCustomerProduct(CustomerView customerproduct);
+	   void deleteIntoCustomerView(long customerId, long productId);
+	   void deleteIntoCustomer(long customerId, long productId);
+	   List<CustomerView> getCustomerByBuyDate(String[] buyDate);
+	   List<CustomerView> getCustomerByName(String customerName);
+	   int getTotalCustomer();
+	   int sumOfQuantity();
+	   double sumOfAmount();
+	   int getTotalProduct();
+	   int getTotalCustomer(String customerName);
+	   int sumOfQuantity(String customerName);
+	   double sumOfAmount(String customerName);
+	   int getTotalProduct(String customerName);
+	   int getTotalCustomer(String[] buyDate);
+	   Integer sumOfQuantity(String[] buyDate);
+	   Double sumOfAmount(String[] buyDate);
+	   int getTotalProduct(String[] buyDate);
+	   int getPresentDate(String buyDate);
+	   int getNumberofCustomersToday(String buyDate);
+	   Double getPresentRevenue(String buyDate);
 }
