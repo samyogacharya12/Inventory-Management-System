8151d3205e4d0f346890703cc5f5e9c1bf1ee356
samyog <samyog.acharya@gmail.com>
2019-09-01 09:05:47 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/Inventory-Management-System
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/demo/model/CustomUserDetails.java
    #	src/main/java/com/example/demo/model/Customer.java
    #	src/main/java/com/example/demo/model/CustomerProduct.java
    #	src/main/java/com/example/demo/model/CustomerView.java
    #	src/main/java/com/example/demo/model/Expense.java
    #	src/main/java/com/example/demo/model/Product.java
    #	src/main/java/com/example/demo/model/Projectuser.java
    #	src/main/java/com/example/demo/model/Purchase.java
    #	src/main/java/com/example/demo/model/PurchaseProduct.java
    #	src/main/java/com/example/demo/model/SupplierProduct.java
    #	src/main/java/com/example/demo/model/SupplierView.java
    #	src/main/java/com/example/demo/model/Trash.java

---
+++ src/main/java/com/example/demo/model/ServiceResponse.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+public class ServiceResponse<T> {
+
+
+    private String status;
+    private T data;
+
+    public ServiceResponse()
+    {
+
+    }
+
+    public ServiceResponse(String status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceResponse{" +
+                "status='" + status + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
