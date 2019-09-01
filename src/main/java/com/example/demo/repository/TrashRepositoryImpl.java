2dfe1cf4dc00ecd856b25de0c030d941d0f92aca
samyog <samyog.acharya@gmail.com>
2019-09-01 08:40:05 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/Inventory-Management-System
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/demo/model/CustomUserDetails.java
    #	src/main/java/com/example/demo/model/Customer.java
    #	src/main/java/com/example/demo/model/CustomerProduct.java
    #	src/main/java/com/example/demo/model/CustomerView.java
    #	src/main/java/com/example/demo/model/Expense.java
    #	src/main/java/com/example/demo/model/Product.java
    #	src/main/java/com/example/demo/model/Projectuser.java
    #	src/main/java/com/example/demo/model/Purchase.java
    #	src/main/java/com/example/demo/model/PurchaseProduct.java
    #	src/main/java/com/example/demo/model/SupplierProduct.java
    #	src/main/java/com/example/demo/model/SupplierView.java
    #	src/main/java/com/example/demo/model/Trash.java

--- src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/TrashRepositoryImpl.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
-import java.util.List;
+import java.util.*;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
@@ -39,6 +40,27 @@ public class TrashRepositoryImpl extends JdbcDaoSupport implements TrashReposito
 		String sql="SELECT * FROM trash";
 		RowMapper<Trash> rowMapper=new BeanPropertyRowMapper<Trash>(Trash.class);
 		return this.getJdbcTemplate().query(sql, rowMapper);
+	}
+
+	@Override
+	public List<Trash> getTrashByProductName(String productName) {
+		String sql="SELECT * FROM trash WHERE product_name=?";
+		List<Map<String, Object>> rows=getJdbcTemplate().queryForList(sql, productName);
+		List<Trash> trashList=new ArrayList<>();
+		for(Map<String, Object> row: rows)
+		{
+			Trash trash=new Trash();
+			trash.setTrashId((Integer) row.get("trash_id"));
+			trash.setProductId((Long) row.get("product_id"));
+			trash.setProductName((String) row.get("product_name"));
+			trash.setProductType((String) row.get("product_type"));
+			trash.setPrice((Double) row.get("price"));
+			trash.setQuantity((Integer) row.get("quantity"));
+			trash.setMagnifactureDate((Date) row.get("magnifacture_date"));
+			trash.setExpiryDate((Date) row.get("expiry_date"));
+			trashList.add(trash);
+		}
+	   	return trashList;
 	}
 
 }
