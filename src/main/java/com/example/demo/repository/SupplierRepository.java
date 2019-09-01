81d4502055bdba91f6600179023d57afe618466f
samyog <samyog.acharya@gmail.com>
2019-09-01 08:37:40 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/Inventory-Management-System
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/demo/model/CustomUserDetails.java
    #	src/main/java/com/example/demo/model/Customer.java
    #	src/main/java/com/example/demo/model/CustomerProduct.java
    #	src/main/java/com/example/demo/model/CustomerView.java
    #	src/main/java/com/example/demo/model/Expense.java
    #	src/main/java/com/example/demo/model/Product.java
    #	src/main/java/com/example/demo/model/Projectuser.java
    #	src/main/java/com/example/demo/model/Purchase.java
    #	src/main/java/com/example/demo/model/PurchaseProduct.java
    #	src/main/java/com/example/demo/model/SupplierProduct.java
    #	src/main/java/com/example/demo/model/SupplierView.java
    #	src/main/java/com/example/demo/model/Trash.java

--- src/main/java/com/example/demo/repository/SupplierRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -8,23 +9,31 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.example.demo.model.Supplier;
 import com.example.demo.model.SupplierProduct;
+import com.example.demo.model.SupplierProductAnalysis;
 import com.example.demo.model.SupplierView;
 
 public interface SupplierRepository {
 
+
+     Supplier getPersonalSupplierByName(String supplierName);
 	 void insertIntoSupplierProduct(SupplierProduct supplierproduct);
+     void insertIntoSupplierProductAnalysis(Map map);
+     List<SupplierProductAnalysis> getAllSupplierProductAnalysis();
+     List<SupplierProductAnalysis> getSupplierProductAnalysisBySupplierId(Integer supplierId);
 	 void insertIntoSupplier(Supplier supplier);
+	 List<SupplierView> getAllSupplierByProductNameAndType(String productName, String productType);
      List<SupplierView> getAllSupplierInfo();
      SupplierView getSupplierBySupplierIdAndProductId(long supplierId, long productId);
+     SupplierView getSupplierBySupplierIdAndUniqueId(long supplierId, long uniqueId);
      Supplier getSupplierId(long supplierId);
      void updateIntoSupplier(Supplier supplier);
      void updateIntoSupplierView(SupplierProduct supplierview);
      void deleteIntoSupplierView(long supplierId, long productId);
      void deleteIntoSupplier(long supplierId, long productId);
-     List<SupplierView> getSupplierByBuyDate(String[] buyDate);
+     List<SupplierView> getSupplierByBuyDate(String supplyStartDate, String supplyLastDate);
      List<Supplier> getAllSupplier();
      List<SupplierView> getSupplierByName(String supplierName);
-     List<SupplierView> getSupplierInformaton();
+     List<SupplierView> getSupplierDistinctName();
      double getSumOfCost();
      int getNoofSupplier();
      int getNoofQuantity();
@@ -34,10 +43,10 @@ public interface SupplierRepository {
      int getNoofSupplier(String supplierName);
      int getNoofQuantity(String supplierName);
      int getNoofProduct(String supplierName);
-    Double getCostFromExpiredProduct(String[] buyDate);
-    Double getSumofCost(String[] buyDate);
-    int getNoofSupplier(String[] buyDate);
-    Integer getNoofQuantity(String[] buyDate);
-     int getNoofProduct(String[] buyDate);
+    Double getCostFromExpiredProduct(String supplyStartDate, String supplyLastDate);
+    Double getSumofCost(String supplyStartDate, String supplyLastDate);
+    int getNoofSupplier(String supplyStartDate, String supplyLastDate);
+    Integer getNoofQuantity(String supplyStartDate, String supplyLastDate);
+     int getNoofProduct(String supplyStartDate, String supplyLastDate);
 }
 
