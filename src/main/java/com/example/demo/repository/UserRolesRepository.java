31bb91c8ba347f196b7791044adf93a257aa5491
samyog <samyog.acharya@gmail.com>
2019-09-01 08:43:40 +0545

    Merge branch 'master' of https://github.com/samyogacharya12/Inventory-Management-System
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/demo/model/CustomUserDetails.java
    #	src/main/java/com/example/demo/model/Customer.java
    #	src/main/java/com/example/demo/model/CustomerProduct.java
    #	src/main/java/com/example/demo/model/CustomerView.java
    #	src/main/java/com/example/demo/model/Expense.java
    #	src/main/java/com/example/demo/model/Product.java
    #	src/main/java/com/example/demo/model/Projectuser.java
    #	src/main/java/com/example/demo/model/Purchase.java
    #	src/main/java/com/example/demo/model/PurchaseProduct.java
    #	src/main/java/com/example/demo/model/SupplierProduct.java
    #	src/main/java/com/example/demo/model/SupplierView.java
    #	src/main/java/com/example/demo/model/Trash.java

--- src/main/java/com/example/demo/repository/UserRolesRepository.java
+++ src/main/java/com/example/demo/repository/UserRolesRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserRolesRepository {
 
+	void insertIntoUserRole(Map map);
+	void updateIntoUserRole(Map map);
+	void deleteIntoUserRole(Map map);
 	 List<String> findRoleByUsername(String username);
 }
